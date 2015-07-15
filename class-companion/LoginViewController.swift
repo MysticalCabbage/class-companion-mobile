ae3484c6b4184c8382f06e15ece9481bfdddb079
JD <jdstep23@gmail.com>
2015-07-14 19:00:42 -0700

    cleaned up extra comments

--- class-companion/LoginViewController.swift
+++ class-companion/LoginViewController.swift
@@ -13,7 +13,6 @@ class LoginViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
   
   @IBOutlet weak var usernameField: UITextField!
